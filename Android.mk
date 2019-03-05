befab9047a26b53dea784e680bd1049c8171dc05
Bruno Martins <bgcngm@gmail.com>
2019-03-05 22:41:14 +0000

    display-hals: Expose c2d2.h even if TARGET_DISABLE_DISPLAY is set
    In commit 0fb9ce3, TARGET_DISABLE_DISPLAY flag was introduced to allow
    opting-out for the compilation of the display modules. It turns out that
    libc2dcolorconvert, which is a media module, depends on the c2d2.h header
    file. Copy the headers, no matter what, so that all media modules can be
    gracefully compiled.
    
    Change-Id: Iadaf007577fb0cb02f79a04078e1f9683a23d01c

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core libdebug
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit libmemtrack hdmi_cec \
+    display-hals += libmemtrack hdmi_cec \
                     $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils
 endif
 
@@ -12,6 +12,10 @@ display-hals += gralloc
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
     display-hals += liblight
 endif
+endif #TARGET_DISABLE_DISPLAY
+ifneq ($(TARGET_IS_HEADLESS), true)
+    display-hals += libcopybit
+endif
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
@@ -19,5 +23,5 @@ else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
+
 endif
-endif #TARGET_DISABLE_DISPLAY
